5dba180c1df80cb8038e9743a27575cf9e1c2bb3
hashtagd3v <jaycoinminer@gmail.com>
2022-03-03 00:17:28 -0600

    Sending email using python script using smtplib, HTML file and substituting variables in the html body.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['from'] = 'Jay Michalek'
 email['to'] = 'recipient@gmail.com'
 email['subject'] = 'Testing python email script from my PC'
 
-email.set_content('I am a Python Master!')
+email.set_content(html.substitute({'name': 'Timmy'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
